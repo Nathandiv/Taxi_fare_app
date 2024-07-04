b11dc65a1ff30f4b8d1db60efacb0149dabc962c
unknown <nathan.hlungwani123@gmail.com>
2024-07-04 23:02:35 +0200

    added fare rate function

---
+++ main.js
@@ -0,0 +1,12 @@
+  // Define fare rates for different routes or destinations
+  const fareRates = {
+    'Pretoria': {
+        'Johannesburg': 60, // Fare from Pretoria to Johannesburg in currency units (e.g., ZAR)
+        // Add more destinations and fares as needed
+    },
+    'Johannesburg': {
+        'Durban': 280, // Fare from Johannesburg to Pretoria
+        //will Add more fares as needed
+    },
+    // will Add more cities or areas with their respective fares
+};
